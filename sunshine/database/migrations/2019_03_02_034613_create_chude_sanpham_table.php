319767f1edffe3de9b9a22dc7ed97e6d397c0343
thanhthinhcusc <48037166+thanhthinhcusc@users.noreply.github.com>
2019-03-01 20:24:37 -0800

    add all table

---
+++ sunshine/database/migrations/2019_03_02_034613_create_chude_sanpham_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChude_sanphamTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('cusc_chude_sanpham', function (Blueprint $table) {
+            $table->unsignedBigInteger('sp_ma')->comment('Sản phẩm # sp_ma # sp_ten # Mã sản phẩm');
+            $table->unsignedTinyInteger('cd_ma')->comment('Chủ để # cd_ma # cd_ten # Mã chủ đề');
+            
+            $table->primary(['sp_ma', 'cd_ma']);
+            $table->foreign('cd_ma')->references('cd_ma')->on('cusc_chude')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('sp_ma')->references('sp_ma')->on('cusc_sanpham')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('cusc_chude_sanpham');
+    }
+}
---
+++ sunshine/database/migrations/2019_03_02_034917_create_hinhanh_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHinhanhTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('cusc_hinhanh', function (Blueprint $table) {
+            $table->unsignedBigInteger('sp_ma')->comment('Sản phẩm # sp_ma # sp_ten # Mã sản phẩm');
+            $table->unsignedTinyInteger('ha_stt')->default('1')->comment('Số thứ tự # Số thứ tự hình ảnh của mỗi sản phẩm');
+            $table->string('ha_ten', 150)->comment('Tên hình ảnh # Tên hình ảnh (không bao gồm đường dẫn)');
+            
+            $table->primary(['sp_ma', 'ha_stt']);
+            $table->foreign('sp_ma')->references('sp_ma')->on('cusc_sanpham')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('cusc_hinhanh');
+    }
+}
