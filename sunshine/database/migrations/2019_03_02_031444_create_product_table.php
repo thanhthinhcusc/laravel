bea530f65c40d3524d2ad49a37f0300601b01b12
thanhthinhcusc <48037166+thanhthinhcusc@users.noreply.github.com>
2019-03-01 19:33:23 -0800

    Update 2019_03_02_031444_create_product_table.php

--- sunshine/database/migrations/2019_03_02_031444_create_product_table.php
+++ sunshine/database/migrations/2019_03_02_031444_create_product_table.php
@@ -16,13 +16,13 @@ class CreateProductTable extends Migration
          Schema::create('product', function (Blueprint $table) {
             $table->bigIncrements('Id');
             $table->string('Name',200);
-            $table->integer('OriginalPrice  ')->default('0');
+            $table->integer('OriginalPrice')->default('0');
             $table->integer('Price')->default('0');
             $table->string('Image',200);
             $table->text('Content');
             $table->string('Description',50);
-            $table->timestamp('DateCreated ')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('DateModified ')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('DateCreated')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('DateModified')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->unsignedTinyInteger('Status')->default('2');
             $table->unsignedTinyInteger('CategoryId');
             $table->foreign('CategoryId')->references('Id')->on('category')->onDelete('cascade')->onUpdate('cascade');
